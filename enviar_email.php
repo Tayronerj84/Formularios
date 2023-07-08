9dd237ec322f1b08334de580f024ba2f24d34a53
Tayronerj84 <138531720+Tayronerj84@users.noreply.github.com>
2023-07-08 16:04:32 -0300

    Update enviar_email.php

--- enviar_email.php
+++ enviar_email.php
@@ -1,10 +1,5 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Configurações de email
-    $to = 'tayrone.santos1120@gmail.com'; // Coloque o endereço de email para onde deseja enviar o formulário
-    $subject = 'Formulário de Hotel';
-    
-    // Dados do formulário
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nomeHotel = $_POST['nome-hotel'];
     $cnpjHotel = $_POST['cnpj-hotel'];
     $enderecoHotel = $_POST['endereco-hotel'];
@@ -13,37 +8,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $emailContato = $_POST['email-contato'];
     $telefoneContato = $_POST['telefone-contato'];
     $cargo = $_POST['cargo'];
-    
-    // Mensagem do email
-    $message = "
-    <html>
-    <head>
-    <title>Detalhes do Formulário</title>
-    </head>
-    <body>
-    <h2>Dados do Hotel:</h2>
-    <p><strong>Nome do Hotel:</strong> $nomeHotel</p>
-    <p><strong>CNPJ do Hotel:</strong> $cnpjHotel</p>
-    <p><strong>Endereço Completo:</strong> $enderecoHotel</p>
-    <p><strong>Rede Hoteleira:</strong> $redeHoteleira</p>
-    <p><strong>Nome do responsável comercial pelo atendimento Globo:</strong> $responsavelGlobo</p>
-    <p><strong>E-mail de contato da pessoa acima:</strong> $emailContato</p>
-    <p><strong>Telefone de contato:</strong> $telefoneContato</p>
-    <p><strong>Cargo:</strong> $cargo</p>
-    </body>
-    </html>
-    ";
-    
-    // Cabeçalhos do email
-    $headers[] = 'MIME-Version: 1.0';
-    $headers[] = 'Content-type: text/html; charset=utf-8';
-    $headers[] = 'From: ' . $emailContato;
-    
-    // Envio do email
-    if (mail($to, $subject, $message, implode("\r\n", $headers))) {
-        echo 'O formulário foi enviado com sucesso.';
-    } else {
-        echo 'Ocorreu um erro ao enviar o formulário.';
-    }
+
+  $destinatario = "tayrone.santos1120@gmail.com"; // Substitua pelo seu endereço de e-mail
+  $assunto = "Informações do formulário";
+    $mensagem = "Nome do Hotel: $nomeHotel\n";
+    $mensagem .= "CNPJ do Hotel: $cnpjHotel\n";
+    $mensagem .= "Endereço Completo: $enderecoHotel\n";
+    $mensagem .= "Rede Hoteleira: $redeHoteleira\n";
+    $mensagem .= "Nome do responsável comercial pelo atendimento Globo: $responsavelGlobo\n";
+    $mensagem .= "E-mail de contato da pessoa acima: $emailContato\n";
+    $mensagem .= "Telefone de contato: $telefoneContato\n";
+    $mensagem .= "Cargo: $cargo\n";
+
+  // Envie o e-mail
+  mail($destinatario, $assunto, $mensagem);
+
+  // Redirecione para uma página de confirmação ou exiba uma mensagem de sucesso
+  echo "As informações foram enviadas por e-mail!";
 }
 ?>
